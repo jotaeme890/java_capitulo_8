e941841d5f5dba93d40e1c98e109ccd325a1c407
jotaeme890 <permanentjavi@gmail.com>
2022-11-25 20:17:54 +0100

    Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,24 @@
+/**
+ * 3 siguientePrimo: Devuelve el menor primo que es mayor al número que se pasa como parámetro.
+ * 
+ * @author jotaeme
+ */
+
+import matematicas.general.*;
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime un número: ");
+        int num = sc.nextInt();
+        if(matematicas.general.varias.esPrimo(num)){
+            System.out.println(num + " es primo, vamos a buscar al siguiente");
+        } else{
+            System.out.println(num + " no es primo, vamos a buscar al siguiente");
+        }
+
+        System.out.println("El siguiente número primo es: "+ matematicas.general.varias.siguientePrimo(num));
+        sc.close();
+    }
+}
