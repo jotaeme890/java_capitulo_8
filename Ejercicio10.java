93489caf4e2003b387570f85bcd269f48c50ee26
jotaeme890 <permanentjavi@gmail.com>
2022-11-26 15:22:22 +0100

    Ejercicio10

---
+++ Ejercicio10.java
@@ -0,0 +1,19 @@
+/**
+ * 10 quitaPorDelante: Le quita a un número n dígitos por delante (por la izquierda).
+ * 
+ * @author jotaeme
+ */
+
+import matematicas.general.*;
+import java.util.Scanner;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime un número: ");
+        int num = sc.nextInt();
+        
+        System.out.println("El número sin el número de detrás: " + matematicas.general.varias.quitaPorDelante(num));
+        sc.close();
+    }
+}
