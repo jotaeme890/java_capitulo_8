2a365bfaade5e72639484b524c5a412341eadb26
jotaeme890 <permanentjavi@gmail.com>
2022-11-26 17:07:21 +0100

    Ejercicio15

---
+++ Ejercicio15.java
@@ -0,0 +1,17 @@
+/**
+ * 15 Muestra los números primos que hay entre 1 y 1000.
+ * 
+ * @author jotaeme
+ */
+
+import matematicas.general.*;
+
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        for(int i = 1; i<1000 ;i++){
+            if(matematicas.general.varias.esPrimo(i)){
+                System.out.println(i);
+            }
+        }
+    }
+}
