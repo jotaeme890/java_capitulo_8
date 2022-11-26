c832a4b54a2810cd61750b65841dc3aa67261241
jotaeme890 <permanentjavi@gmail.com>
2022-11-26 17:14:09 +0100

    Ejercicio16

---
+++ Ejercicio16.java
@@ -0,0 +1,17 @@
+/**
+ * 16 Muestra los números capicúa que hay entre 1 y 99999.
+ * 
+ * @author jotaeme
+ */
+
+import matematicas.general.*;
+
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        for(int i = 1; i<1000 ;i++){
+            if(matematicas.general.varias.esCapicuo(i)){
+                System.out.println(i);
+            }
+        }
+    }
+}
