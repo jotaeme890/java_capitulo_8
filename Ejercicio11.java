5d0701ab4345df6fa50ff8daaa7145b225495588
jotaeme890 <permanentjavi@gmail.com>
2022-11-26 15:38:17 +0100

    Ejercicio11

---
+++ Ejercicio11.java
@@ -0,0 +1,20 @@
+/**
+ * 11 pegaPorDetras: Añade un dígito a un número por detrás.
+ * 
+ * @author jotaeme
+ */
+
+import matematicas.general.*;
+import java.util.Scanner;
+
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime un número: ");
+        int num = sc.nextInt();
+        System.out.print("Dime el número a añadir: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("El número " + num + " con el añadido sería: " + matematicas.general.varias.pegaPorDetras(num, num2));
+    }
+}
