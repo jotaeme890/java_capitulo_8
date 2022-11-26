f984da309ed63c908f6ab869f99b2a79e53e9875
jotaeme890 <permanentjavi@gmail.com>
2022-11-26 16:45:59 +0100

    Ejercicio14

---
+++ Ejercicio14.java
@@ -0,0 +1,20 @@
+/**
+ * 14 juntaNumeros: Pega dos números para formar uno.
+ * 
+ * @author jotaeme
+ */
+
+import matematicas.general.*;
+import java.util.Scanner;
+
+public class Ejercicio14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime un número: ");
+        int num = sc.nextInt();
+        System.out.print("Dime el número a añadir: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("El número " + num + " con el añadido sería: " + matematicas.general.varias.juntaNumeros(num, num2));
+    }
+}
