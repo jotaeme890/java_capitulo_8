56a97112c732eeed2529970462be4c8c2d73096b
jotaeme890 <permanentjavi@gmail.com>
2022-11-26 13:36:09 +0100

    Ejercicio4

---
+++ Ejercicio4.java
@@ -0,0 +1,21 @@
+/**
+ * 4 potencia: Dada una base y un exponente devuelve la potencia.
+ * 
+ * @author jotaeme
+ */
+
+import matematicas.general.*;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime un número: ");
+        int num = sc.nextInt();
+        System.out.print("Dime la potencia: ");
+        int pot = sc.nextInt();
+
+        System.out.println("El número " + num + " potenciado a " + pot + ", es: " + matematicas.general.varias.potencia(num,pot));
+        sc.close();
+    }
+}
