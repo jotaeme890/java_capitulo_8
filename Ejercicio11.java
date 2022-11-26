34dfa3a1b142936130c9438049bceb7a1a4195da
jotaeme890 <permanentjavi@gmail.com>
2022-11-26 15:42:34 +0100

    Ejercicio12

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -16,5 +16,6 @@ public class Ejercicio11 {
         int num2 = sc.nextInt();
 
         System.out.println("El número " + num + " con el añadido sería: " + matematicas.general.varias.pegaPorDetras(num, num2));
+        sc.close();
     }
 }
