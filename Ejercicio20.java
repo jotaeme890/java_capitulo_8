0fee1e84873636e2ad6e3f30ee38dcb79f14960f
jotaeme890 <permanentjavi@gmail.com>
2022-11-27 14:35:53 +0100

    Ejercicio20

---
+++ Ejercicio20.java
@@ -0,0 +1,29 @@
+/**
+ * 20 generaArrayInt: Genera un array de tamaño n con números aleatorios
+ * cuyo intervalo (mínimo y máximo) se indica como parámetro.
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+import matematicas.general.*;
+
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime el tamaño del array: ");
+        int n = sc.nextInt();
+        System.out.print("Dime el mínimo del intervalo: ");
+        int min = sc.nextInt();
+        System.out.print("Dime el máximo del intervalo: ");
+        int max = sc.nextInt();
+
+        int[] num = matematicas.general.arrays.generaArrayInt(n, min, max);
+
+        for(int i = 0; i<n;i++){
+            System.out.print(num[i] + " ");
+        }
+
+        sc.close();
+    }
+}
