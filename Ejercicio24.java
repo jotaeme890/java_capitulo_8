e7020942ecd6a81cece9273e064a6825460e4de5
jotaeme890 <permanentjavi@gmail.com>
2022-11-27 15:00:49 +0100

    Ejercicio24

---
+++ Ejercicio24.java
@@ -0,0 +1,29 @@
+/**
+ * 24  estaEnArrayInt: Dice si un número está o no dentro de un array.
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+import matematicas.general.*;
+
+public class Ejercicio24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime el tamaño del array: ");
+        int n = sc.nextInt();
+        int[] num = new int [n];
+
+        for(int i = 0; i<num.length;i++){
+            System.out.print("Dime un número para el array: ");
+            num [i] = sc.nextInt();
+        }
+
+        System.out.print("Dime el número que quieres saber si está en el array o no: ");
+        int n2 = sc.nextInt();
+
+        System.out.print("La media de ese array es: " + matematicas.general.arrays.estaEnArrayInt(num,n));
+
+        sc.close();
+    }
+}
