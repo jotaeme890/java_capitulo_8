5a51cae1ec2ea37f3155338b0ed00341a147844a
jotaeme890 <permanentjavi@gmail.com>
2022-11-28 09:53:47 +0100

    Ejercicio18

---
+++ Ejercicio18.java
@@ -0,0 +1,23 @@
+/**
+ * 17 Escribe un programa que pase de decimalo a binario.
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+import matematicas.general.*;
+
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime un número decimal: ");
+        int decimal= sc.nextInt();
+
+        String binario2 = Integer.toBinaryString(decimal);
+        
+        System.out.print("El número " + decimal + " en binario es: " + binario2);
+
+        sc.close();
+        sc.close();
+    }
+}
