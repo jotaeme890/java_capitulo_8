9e70af11e1661fd933c865f68f7548f5f74561ad
jotaeme890 <permanentjavi@gmail.com>
2022-11-28 17:22:29 +0100

    Ejercicio25

---
+++ Ejercicio25.java
@@ -0,0 +1,30 @@
+/**
+ * 25  posicionEnArray: Busca un número en un array y devuelve la posición (el índice) en la que se encuentra.
+
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+import matematicas.general.*;
+
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime el tamaño del array: ");
+        int n = sc.nextInt();
+        int[] num = new int [n];
+
+        for(int i = 0; i<num.length;i++){
+            System.out.print("Dime un número para el array: ");
+            num [i] = sc.nextInt();
+        }
+
+        System.out.print("Dime el número que quieres saber si está en el array o no: ");
+        int n2 = sc.nextInt();
+
+        System.out.print("El número " + n2 + " está en la posición: " + matematicas.general.arrays.posicionEnArray(num,n2));
+
+        sc.close();
+    }
+}
