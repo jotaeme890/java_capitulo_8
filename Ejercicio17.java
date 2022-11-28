89d8177ad410b1d403efe0251699663f5bfa3cdb
jotaeme890 <permanentjavi@gmail.com>
2022-11-28 17:03:37 +0100

    Ejercicio17

--- Ejercicio17.java
+++ Ejercicio17.java
@@ -11,12 +11,9 @@ public class Ejercicio17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.print("Dime un número binario: ");
-        int binario= sc.nextInt();
-
-        String binario2 = Integer.toBinaryString(binario);
-        int decimalCon = Integer.parseInt(binario2,2);
+        String binario= sc.next();
         
-        System.out.print("El número " + binario + " en decimal es: " + decimalCon);
+        System.out.print("El número " + binario + " en decimal es: " + matematicas.general.varias.aDecimal(binario));
 
         sc.close();
     }
